0ea864dc6c8ecd0231ef8a3e45b4fd19d64dca53
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-03 15:07:41 +0100

    update project to new course section

--- src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AboutPage () {
-  return (
-    <main>
-      <h1>About us</h1>
-    </main>
-  )
-}
--- src/app/blog/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-type Props = { params: { slug: string } }
-
-export default function BlogPostPage({params}: Props) {
-  return <main>
-    <h1>Blog Post</h1>
-    <p>{params.slug}</p>
-  </main>
-}
