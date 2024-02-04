9cadc5a384bf807434fa723a1ea4447ee33ff93c
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-04 14:05:54 +0100

    add image-slideshow component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 
 import styles from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.slideshow}></div>
+        <div className={styles.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={styles.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
