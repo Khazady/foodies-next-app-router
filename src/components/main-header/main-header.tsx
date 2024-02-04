5c0e03c26ab4d589493e0ae29f1e921bfee6a957
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-04 15:45:32 +0100

    isolate client nav-link logic to a separate component, to minify client code

--- src/components/main-header/main-header.tsx
+++ src/components/main-header/main-header.tsx
@@ -5,6 +5,7 @@ import logoImg from "@/assets/logo.png";
 
 import styles from "./main-header.module.css";
 import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
 
 export default function MainHeader() {
   return (
@@ -19,10 +20,10 @@ export default function MainHeader() {
         <nav className={styles.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals"> Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community"> Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
---
+++ src/components/main-header/nav-link.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import styles from "./nav-link.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+type Props = {
+  href: string;
+  children: ReactNode;
+};
+
+export default function NavLink(props: Props) {
+  const { href, children } = props;
+  const path = usePathname();
+  return (
+    <Link
+      href={href}
+      className={
+        path.startsWith(href) ? `${styles.active} ${styles.link}` : styles.link
+      }
+    >
+      {children}
+    </Link>
+  );
+}
