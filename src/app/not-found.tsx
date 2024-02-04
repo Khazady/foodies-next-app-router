e08ab873bad46c2c20126b648dcc38d9957e8281
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-05 00:46:44 +0100

    add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, we were not able to found page by this route.</p>
+    </main>
+  );
+}
