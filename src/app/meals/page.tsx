d88e5b366388fee927703c3e5f5127a39581a753
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-05 00:34:24 +0100

    isolate layout which is loading by using React.Suspense

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -2,9 +2,14 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
@@ -20,7 +25,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
