1d56d6af38068c9c05b045189abdcdc8ecd0b353
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-05 15:55:45 +0100

    minor: add more specific error for share/ path

---
+++ src/app/meals/share/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function MealsErrorPage(
+  props: Readonly<{
+    error: Error & { digest?: string };
+    reset: () => void;
+  }>,
+) {
+  console.log(props.error.message);
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to create meal.</p>
+    </main>
+  );
+}
