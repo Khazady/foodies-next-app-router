ba5b4c2a14d8ba0e6c8708d37b221afc55c00e14
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-05 15:55:25 +0100

    implement simple form validation

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,6 +3,27 @@
 import { MealFormData, saveMeal } from "@/lib/meals";
 import { redirect } from "next/navigation";
 
+function isInvalidFormValue(
+  formValue: ReturnType<FormData["get"]>,
+  options?: { emailCheck?: boolean },
+) {
+  if (!formValue) {
+    return false;
+  }
+  if (formValue instanceof String) {
+    if (formValue.trim() === "") {
+      return false;
+    }
+    if (options?.emailCheck && !formValue.includes("@")) {
+      return false;
+    }
+  }
+  if (formValue instanceof File && formValue.size === 0) {
+    return false;
+  }
+  return true;
+}
+
 export async function shareMeal(formData: FormData) {
   const meal = {
     title: formData.get("title"),
@@ -12,6 +33,17 @@ export async function shareMeal(formData: FormData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidFormValue(meal.title) ||
+    isInvalidFormValue(meal.summary) ||
+    isInvalidFormValue(meal.image) ||
+    isInvalidFormValue(meal.instructions) ||
+    isInvalidFormValue(meal.creator) ||
+    isInvalidFormValue(meal.creator_email, { emailCheck: true })
+  ) {
+    throw new Error("Invalid input");
+  }
   await saveMeal(meal as MealFormData);
   redirect("/meals");
 }
