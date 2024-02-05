35fc413dc97123978177e4b12e1b2ebe6239024d
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-05 14:51:39 +0100

    implement saveMeal server action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { MealFormData, saveMeal } from "@/lib/meals";
+
 export async function shareMeal(formData: FormData) {
   const meal = {
     title: formData.get("title"),
@@ -7,6 +9,7 @@ export async function shareMeal(formData: FormData) {
     summary: formData.get("summary"),
     instructions: formData.get("instructions"),
     creator: formData.get("name"),
-    creatorEmail: formData.get("email"),
+    creator_email: formData.get("email"),
   };
+  await saveMeal(meal as MealFormData);
 }
--- src/lib/meals.ts
+++ src/lib/meals.ts
@@ -1,5 +1,10 @@
+import fs from "node:fs";
+
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 import { Meal } from "~/initdb";
+import { randomUUID } from "node:crypto";
 
 const db = sql("meals.db");
 
@@ -12,8 +17,43 @@ export async function getMeals() {
 }
 
 export function getMeal(slug: Meal["slug"]) {
-  // ? and .get() to protect from SQL injection attack
-  return db
-    .prepare("SELECT * FROM meals WHERE slug = ?")
-    .get(slug) as unknown as Meal;
+  // ? and .get() to protect from SQL injection attack, in better-sqlite3 syntax it can be replaced like this @slug
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug) as Meal;
+}
+
+export type MealFormData = Omit<Meal, "image" | "slug"> & { image: File };
+
+export async function saveMeal(meal: MealFormData) {
+  const slug = slugify(meal.title, { lower: true });
+  //sanitize html to prevent XSS attack
+  const instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${slug}_${randomUUID()}.${extension}`;
+
+  // save file to public/images folder
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Saving image failed!");
+    }
+  });
+  // replace File with path to it, (public folder is already root, so we don't indicate it here
+  const imagePath = `/images/${fileName}`;
+  db.prepare(
+    `
+  INSERT INTO meals
+    (title, summary, instructions, creator, creator_email, image, slug)
+    VALUES (
+         @title,
+         @summary,
+         @instructions,
+         @creator,
+         @creator_email,
+         @image,
+         @slug
+    )
+  `,
+  ).run({ ...meal, image: imagePath, slug, instructions });
 }
