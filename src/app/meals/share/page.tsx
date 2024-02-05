50a90cb5fcaa62dfe4cb8250faf6a8ba23fa6cf8
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-05 12:52:32 +0100

    handle form with server action

--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -2,6 +2,19 @@ import styles from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
 
 export default function ShareMealPage() {
+  async function shareMeal(formData: FormData) {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      image: formData.get("image"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      creator: formData.get("name"),
+      creatorEmail: formData.get("email"),
+    };
+  }
+
   return (
     <>
       <header className={styles.header}>
@@ -11,7 +24,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        <form className={styles.form} action={shareMeal}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +52,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="" name={""} />
+          <ImagePicker label="Your image" name="image" />
           <p className={styles.actions}>
             <button type="submit">Share Meal</button>
           </p>
