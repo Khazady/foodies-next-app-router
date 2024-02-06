7adc217ab2696c6e14e36b4059aa2d4d76384a93
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-06 19:39:21 +0100

    add nested metadata to meals page

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -4,6 +4,11 @@ import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 
+export const metadata = {
+  title: "All Meals",
+  description: "All meals, shared by a food-loving community.",
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
